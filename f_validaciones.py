b558d6a51ec306e4f4901bd811092b62e13de3d7
Andres Morillas <patomorillas@gmail.com>
2025-10-27 14:30:29 -0300

    Funciones para validar paises y strings basicas

---
+++ f_validaciones.py
@@ -0,0 +1,11 @@
+def validar_nombre(nombre_p :str) -> str : # type: ignore
+    
+    if nombre_p == "" or not(nombre_p.isalpha()):
+        print("No se pueden ingresar numeros, o dejar el campo vacio")
+        while (nombre_p == "" or not(nombre_p.isalpha())):
+            nombre_p :str = input("Que pais desea buscar? ")
+    
+    return nombre_p
+
+
+if __name__ is 
